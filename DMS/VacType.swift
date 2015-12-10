47bea0c26c8567f9a0e2aaddbdf0a06145508278
Mohamed Fadl <comulink_mac_1@amrs-imac.local>
2015-12-10 17:37:40 +0300

    test

---
+++ DMS/VacType.swift
@@ -0,0 +1,19 @@
+//
+//  VacType.swift
+//  VMS
+//
+//  Created by Mohamed Fadl on 12/10/15.
+//  Copyright © 2015 Compulink. All rights reserved.
+//
+
+import Foundation
+
+class VacType {
+    var idx:Int
+    var title:String
+    
+    init(idx:Int, title:String){
+        self.idx = idx
+        self.title = title
+    }
+}
